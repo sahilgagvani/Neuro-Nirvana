ce84be92fcf320e32ac3c1321f8c781b56f0b0d8
mary-emma <maryemmabarnhill@gmail.com>
2024-03-28 14:06:54 -0400

    Added scroll feature, changed menu options to buttons for grouping

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    highlighted = 1;
+    currScreen = "Menu";
+    menuStrings.push_back("Menu Options:"); // placeholder option to fill menuStrings[0] so that highlighted is the index of the correct option in menuStrings
+    menuStrings.push_back("New Session");
+    menuStrings.push_back("Session Log");
+    menuStrings.push_back("Time and Date");
+    connect(ui->upArrowButton, SIGNAL(pressed()), this, SLOT(scroll()));
+    connect(ui->downArrowButton, SIGNAL(pressed()), this, SLOT(scroll()));
+
+    foreach(QAbstractButton *button, ui->menuOptions->buttons()){
+        ui->menuOptions->setId(button, button->objectName().right(1).toInt());
+    }
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +25,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::scroll() {
+    QObject *senderButton = sender();
+    ui->menuOptions->button(highlighted)->setStyleSheet("QPushButton{background-color: rgb(153, 193, 241); padding: 0px 25px 0px 25px; border: none; text-align: left; }\n");
+    if (senderButton->objectName().left(2) == "up") {
+        highlighted -= 1;
+        if (highlighted < 1) {
+            highlighted = 3;
+        }
+    } else {
+        highlighted += 1;
+        if (highlighted > 3) {
+            highlighted = 1;
+        }
+    }
+    ui->menuOptions->button(highlighted)->setStyleSheet("QPushButton{background-color: rgb(154, 153, 150); padding: 0px 25px 0px 25px; border: none; text-align: left; }\n");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -14,8 +14,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void scroll();
 
 private:
     Ui::MainWindow *ui;
+    std::list<QString> menuStrings; // list of the menu options displayed on the screen
+    int highlighted; // holds the index of the currently selected menu options
+    QString currScreen; // holds the current screen - menu, new session, etc.
 };
 #endif // MAINWINDOW_H
