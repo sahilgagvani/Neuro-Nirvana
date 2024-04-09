6547de883c1dad817ab6135ff8f2d30c947aedf6
ThatOneGuy36 <148909630+ThatOneGuy36@users.noreply.github.com>
2024-04-09 17:01:00 -0400

    Updated CentralProcessor.h

---
+++ CentralProcessor.h
@@ -0,0 +1,34 @@
+#ifndef CENTRALPROCESSOR_H
+#define CENTRALPROCESSOR_H
+
+#include "Electrode.h"
+#include <QObject>
+#include <list>
+
+class CentralProcessor : public QObject {
+    Q_OBJECT;
+
+    private:
+        std::list<Electrode> electrodes;
+
+        int numTreatments = 4;
+        int numElectrodes = 7;
+        int offsetFreq = 5;
+
+    public:
+        CentralProcessor(QObject *parent = nullptr);
+        ~CentralProcessor();
+
+        // Dominant frequency for a single electrode
+        double calculateDominantFrequency(Electrode*);
+
+        // Baseline frequency is the average of all dominant frequencies
+        double calculateBaselineFrequency();
+
+        void applyTreatmentRound();
+
+        void saveData();
+
+};
+
+#endif // CENTRALPROCESSOR_H
